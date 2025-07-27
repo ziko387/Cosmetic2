01009f5f478c14d6c7e8a56dbf7833924485c11e
ziko387 <mwangiian973@gmail.com>
2025-07-27 15:56:33 +0300

    added a file

--- cosmetic/ComsmeticApp/models.py
+++ cosmetic/ComsmeticApp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class cosmeticItems(models.Model):
-    item =models.characterField(max_length=100, unique=True),
-    price = models.DecimalField(max_digits=10, decimal_places=2),
-    description = models.TextField(blank=True, null=True,unique= True),
-    image = models.ImageField(upload_to='cosmetic_images/', blank=True, null=True),
-    category = models.CharField(max_length=50,choices=[
+    item = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True, unique=True)
+    image = models.ImageField(upload_to='cosmetic_images/', blank=True, null=True)
+    category = models.CharField(max_length=50, choices=[
         ('skincare', 'Skincare'),
         ('makeup', 'Makeup'),
         ('haircare', 'Haircare'),
@@ -14,20 +15,42 @@ class cosmeticItems(models.Model):
         ('tools', 'Tools'),
         ('nails', 'Nails'),
     ])
-    Area= models.characterfield(max_length=120,unique=True),
-    created_at = models.DateTimeField(auto_now_add=True),
-    updated_at = models.DateTimeField(auto_now=True),
+    Area = models.CharField(max_length=120, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.item
         
-class  Order(models.models):
+class Order(models.Model):
     item= models.ForeignKey(cosmeticItems, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)   
     userEmail = models.EmailField()
     shipping_address = models.CharField(max_length=255)
     order_date = models.DateTimeField(auto_now_add=True)     
-    def _str_(self):
-        return f"Order {self.id} - {self.item.item} - {self.quantity} pcs -{self.order_date.strftime('%Y-%m-%d %H:%M:%S')}"
+    def __str__(self):
+        return f"Order {self.id}-{self.item.item}-{self.quantity}pcs-{self.order_date.strftime('%Y-%m-%d %H:%M:%S')}"
+    
+class user(models.Model):
+    username= models.CharField(max_length=170, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    phonenumber = models.CharField(max_length=15, blank=True, null=True)
+
+
+class userProfile(models.Model):
+    user = models.OneToOneField(user, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile -{self.profile_picture.url if self.profile_picture else 'No Picture'}"
+     
+    
+
+
   
 
